3cad57b993ae66ac4889a055bdb27495759c6cec
Nurullo <khuwaidullaev@gmail.com>
2021-03-20 01:31:19 +0500

    Developing a DOM Updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,6 +21,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // Update results view to mark selected search results
+    resultsView.update(model.getSearchResultsPage())
+
     // 1) Loading recipe 
     await model.loadRecipe(id);
     // const { recipe } = model.state;
@@ -64,7 +67,8 @@ const controlPagination = function(goToPage) {
 
 const controlServings = function(newServings) {
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 }
 
 const init = function() {
